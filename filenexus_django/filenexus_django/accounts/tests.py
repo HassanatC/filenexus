be64a97c3fd3cbdee8cc54e1f27fb9d31b185f7c
HassanatC <hassanat431@hotmail.co.uk>
2023-09-17 09:51:32 +0100

    final updates

--- filenexus_django/filenexus_django/accounts/tests.py
+++ filenexus_django/filenexus_django/accounts/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
 
 # Create your tests here.
+<<<<<<< HEAD
+=======
+
+class FileUploadTest(APITestCase):
+
+    def setUp(self):
+        
+        self.user = get_user_model().objects.create_user(
+            username='testuser10',
+            password='testpassword10',
+            email='testuser10@email.com'
+        )
+        self.user.is_active = True
+        self.user.save()
+        
+      
+        self.token = Token.objects.create(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Token {self.token.key}')
+        
+    def test_file_upload(self):
+        file = SimpleUploadedFile("testfile.txt", b"file_content", content_type="text/plain")
+        
+        response = self.client.post(reverse('upload_file'), {'file': file}, format='multipart')
+        
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
+        self.assertTrue(File.objects.filter(name="testfile.txt").exists())
+        
+
+    def tearDown(self):
+        File.objects.filter(name="testfile.txt").delete()
+>>>>>>> 8025531 (final updates)
